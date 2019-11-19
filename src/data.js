535a019aa6e409c7ecb9d4f886a1a9ddd2475261
Jayant Rai <jayant.chamling@outlook.com>
2019-11-18 17:03:55 -0800

    added images and data

---
+++ src/data.js
@@ -0,0 +1,102 @@
+export const menuItems = [
+    {
+        id: 1, 
+        title: "Chicken and rice",
+        img: "../public/img/menu1.jpeg",
+        price: 9.99,
+        description: "Rice topped with chicken gravy",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2, 
+        title: "Shrimp curry with rice",
+        img: "../public/img/menu2.jpeg",
+        price: 11.99,
+        description: "Fresh Shrimp topped with rice and curry",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 3, 
+        title: "Beef Burger",
+        img: "../public/img/menu3.jpeg",
+        price: 9.99,
+        description: "Beef patty topped with lettuce, pickles, tomatoes",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 4, 
+        title: "Fruit Salad",
+        img: "../public/img/menu4.jpeg",
+        price: 9.99,
+        description: "Variety of fruits",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 5, 
+        title: "Pork slice",
+        img: "../public/img/menu5.jpeg",
+        price: 9.99,
+        description: "Pork chopped with cilantro",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 6, 
+        title: "Chicken kabab",
+        img: "../public/img/menu6.jpeg",
+        price: 9.99,
+        description: "Deep Fried chicken",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 7, 
+        title: "Chicken Sphagetti",
+        img: "../public/img/menu7.jpeg",
+        price: 9.99,
+        description: "Chicken served with sphagetti",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 8, 
+        title: "Turkey Sandwich",
+        img: "../public/img/menu8.jpeg",
+        price: 9.99,
+        description: "Turkey Sandwich with lettuce",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 9, 
+        title: "Seafood Noodle",
+        img: "../public/img/menu9.jpeg",
+        price: 9.99,
+        description: "Served with noodle and egg",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 1, 
+        title: "Shrimp Salad",
+        img: "../public/img/menu10.jpeg",
+        price: 9.99,
+        description: "Shrimp served with lettuce",
+        inCart: false,
+        count: 0,
+        total: 0
+    }
+]
