9269060d42c92156c1e21adecf27bebc893a7d72
pineappl-e <tiwariapar>
2019-11-26 19:03:37 -0800

    chamnged app.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,35 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import NavBar from './Components/NavBar.js'
-import MenuList from './Components/MenuList.js'
-import AboutUs from './Components/AboutUs.js'
-import Cart from './Components/Cart/Cart'
-import Modal from './Components/Modal'
-import Details from './Components/Details'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './Components/NavBar.js';
+import MenuList from './Components/MenuList.js';
+import AboutUs from './Components/AboutUs.js';
+import Cart from './Components/Cart/Cart';
+import Modal from './Components/Modal';
+import Details from './Components/Details';
 
+// Hello World
 
-
-
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
-
-
-  render() {
-  return (
-    <div>
-     
-            <Router>
-            <NavBar />
-              <Switch>
-                    <Route exact path="/" component={AboutUs} />
-                    <Route path="/menu" component={MenuList} />
-                    <Route path="/details" component={Details} />
-                    <Route path="/aboutus" component={AboutUs} />
-                    <Route path="/cart" component={Cart} />
-                    
-           
-            
-              </Switch>
-            <Modal />
-            </Router> 
-           
-      </div>
-
-          
-
-  );
-}}
+	render() {
+		return (
+			<div>
+				<Router>
+					<NavBar />
+					<Switch>
+						<Route exact path="/" component={AboutUs} />
+						<Route path="/menu" component={MenuList} />
+						<Route path="/details" component={Details} />
+						<Route path="/aboutus" component={AboutUs} />
+						<Route path="/cart" component={Cart} />
+					</Switch>
+					<Modal />
+				</Router>
+			</div>
+		);
+	}
+}
 
 export default App;
